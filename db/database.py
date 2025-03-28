5ff0ac9cac61155f23591206740360b63ed95641
N1k1f0rM <nikifortch53@gmail.com>
2025-03-29 00:56:37 +0300

    add database structure and migrations

---
+++ db/database.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Integer, Float, TIMESTAMP, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from datetime import datetime
+from config import DB_USER, DB_PASS, DB_HOST, DB_NAME, DB_PORT
+
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Cars(Base):
+    __tablename__ = "cars"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    time: Mapped[datetime.timestamp] = mapped_column(TIMESTAMP, server_default=func.now(), nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    driven: Mapped[int] = mapped_column(Integer, nullable=False)
+    mileage: Mapped[float] = mapped_column(Float, nullable=False)
+    engine: Mapped[int] = mapped_column(Integer, nullable=False)
+    hp: Mapped[float] = mapped_column(Float, nullable=False)
+    seats: Mapped[int] = mapped_column(Integer, nullable=False)
+    price: Mapped[float] = mapped_column(Float, nullable=False)
+
+
+engine = create_async_engine(DATABASE_URL)
+session = async_sessionmaker(engine, expire_on_commit=False)
